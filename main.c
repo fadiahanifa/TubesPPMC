a8a4d6dcdb99a76acf8dfb7d0e1efeef8c1850c0
fadiahanifa <60212665+fadiahanifa@users.noreply.github.com>
2020-04-06 20:48:28 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020 * MODUL 8 – TUGAS BESAR
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020 * MODUL 8 â€“ TUGAS BESAR
 * Kelompok          : 3
 * Hari dan Tanggal  : Senin, 6 April 2020
 * Asisten (NIM)     : Rahma Rizky Alifia (18316003)
@@ -9,60 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tick.c"
 
 void read_file();
-void tick(int row, int col, int arr[row][col]);
-void animate(int row, int col, int arr[row][col]);
-
-void tick(int row, int col, int arr[row][col]){
-//KAMUS
-    int arr_nextgen[row][col];
-    int i, j, m, n;
-//ALGORITMA
-    for (i = 0; i < row; i++) {
-        for (j = 1; j < col; j++){
-            int neighbor_alive = 0;
-            for (m = -1; m <= 1; m++){
-                for (n = -1; n <= 1; n++){
-                    neighbor_alive += arr[((i + m + row) % row)][((j + n + col) % col)];
-                }
-            }
-            neighbor_alive -= arr[i][j];
-            if (arr[i][j]) {
-                if ((neighbor_alive >= 2) && (neighbor_alive < 4)){
-                    arr_nextgen[i][j] = 1;
-                }
-                else {
-                    arr_nextgen[i][j] = 0;
-                }
-            }
-            else {
-                if (neighbor_alive == 3){
-                    arr_nextgen[i][j] = 1;
-                }
-                else {
-                    arr_nextgen[i][j] = 0;
-                }
-            }
-        }
-    }
-    for (i = 0; i < row; i++){
-        for (j = 0; j < col; j++){
-            arr[i][j] = arr_nextgen[i][j];
-        }
-    }
-    free(arr_nextgen);
-    // fungsi print
-}
-
-void animate(int row, int col, int arr[row][col]){
-//ALGORITMA
-    for (int i = 0; i < 30; i++){
-        tick(row, col, arr);
-    }
-}
-
-
 int main(){
 //KAMUS
     int menu, out;
@@ -74,7 +23,7 @@ awal:
     printf("");
     printf("\n\nMenu:");
     printf("\nMasukkan file seed : ");
-    gets(file_name);
+    scanf("%s", &file_name);
     fptr=fopen(file_name,"r");
     //read_file();
     do{
@@ -85,10 +34,10 @@ awal:
         scanf("%d",&menu);
         switch(menu){
             case 1:
-                //animate(int row, int col, int arr[row][col]);
+                animate(int row, int col, int arr[][col]);
                 break;
             case 2:
-                //tick(int row, int col, int arr[row][col]);
+                tick(int row, int col, int arr[][col]);
                 break;
             case 3:
                 printf("\nApakah Anda ingin memasukkan file seed baru (y/t)?\n");
