0c592605df00907bf178438d990a0b520452f3da
itsjustalam <49231154+itsjustalam@users.noreply.github.com>
2020-04-08 17:56:45 +0800

    Update readfile.c

--- readfile.c
+++ readfile.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double getRandomDoubleInRange(double min, double max)
+{
+    return min + (max-min) * ((double)rand() / RAND_MAX);   
+    /*  Fungsi untuk memperoleh nilai random tipe float
+        dalam rentang min - max.
+        Kenapa float, bukan integer ? 
+            Bila user memberi input (min, max) = (0, 1),
+            fungsi ini dapat digunakan.
+        */
+}
+
+// (2)
+
 char* create(int rows, int columns)
 {
     char* simulation = (char*)calloc(rows*columns, sizeof(char));
@@ -10,7 +26,7 @@ char* create(int rows, int columns)
     {
         for(int x = 1; x < columns - 1; x++)    // (**)
         {
-            if (getRandomDoubleInRange(0.0, 1.0) <= 0.35)
+            if (getRandomDoubleInRange(0.0, 1.0) <= 0.35)           // Misalkan seeding kalo dapet angka >35%, sel hidup
             {
                 *(simulation + y * columns + x) = '-';  // Sel mati
             }
