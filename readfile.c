b100e8040a2e6f8b89d69752da69878c3efe3d04
itsjustalam <49231154+itsjustalam@users.noreply.github.com>
2020-04-09 08:54:36 +0800

    Update readfile.c

--- readfile.c
+++ readfile.c
@@ -8,64 +8,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char* argv[]) 
-    /*  argc : number of arguments passing the command line
-        argv : argument vector
+int readfile(FILE file_name, ...)
+{
+    // KAMUS
+    
+    
+    
+    // ALGORITMA
+    
+    // Read file dapat dilakukan apabila file tidak NULL
+    if (file_name == NULL) 
+    {
+        return NULL;    
+    }
+
+    /*  Nilai baris (row) dan kolom (column) telah
+        tertera pada text file uji
     */
-{
-    int rows = atoi(argv[1]);
-    if(rows <= 0)
+    row = ...
+    col = ...
+    
+    /*  Mulai dari baris ke-3 pada text (gen. 1 simulasi),
+        
+    */
+        
+    for (int j = 0; j < col; j++)
     {
-        printf("Baris harus > 0. Was %d\n", rows);
-        return -1;
-    }
-    rows += 2;      // (*)
-
-    int columns = atoi(argv[2]);
-    if(rows <= 0)
-    {
-        printf("Columns count must be greater than zero. Was %d\n", columns);
-        return -1;
-    }
-    columns += 2;   // (*)
-}
-
-
-double getRandomDoubleInRange(double min, double max)
-{
-    return min + (max-min) * ((double)rand() / RAND_MAX);   
-    /*  Fungsi untuk memperoleh nilai random tipe float
-        dalam rentang min - max.
-        Kenapa float, bukan integer ? 
-            Bila user memberi input (min, max) = (0, 1),
-            fungsi ini dapat digunakan.
-        */
-}
-
-// (2)
-
-char* create(int rows, int columns)
-{
-    char* simulation = (char*)calloc(rows*columns, sizeof(char));
-    if(simulation == NULL)
-    {
-        return NULL;
-    }
-
-    for (int y = 1; y < rows - 1; y++)          // (**)
-    {
-        for(int x = 1; x < columns - 1; x++)    // (**)
+        for (int i = 0; i < row; i++)
         {
-            if (getRandomDoubleInRange(0.0, 1.0) <= 0.35)           // Misalkan seeding kalo dapet angka >35%, sel hidup
-            {
-                *(simulation + y * columns + x) = '-';  // Sel mati
-            }
-            else
-            {
-                *(simulation + y * columns + x) = 'X';  // Sel hidup
-            }
+            fscanf(filename, "%s", arr[i][j]);     
         }
     }
-
-    return simulation;
 }
