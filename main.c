a1694d565732c16fdd5d677d9a4c2571d8938ab0
fadiahanifa <60212665+fadiahanifa@users.noreply.github.com>
2020-04-09 11:45:09 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020 * MODUL 8 – TUGAS BESAR
 * Kelompok          : 3
 * Hari dan Tanggal  : Kamis, 9 April 2020
-		      diedit pada Rabu, 8 April 2020
 * Asisten (NIM)     : Rahma Rizky Alifia (18316003)
 * Nama File         : main.c
 * Deskripsi         : Program utama
